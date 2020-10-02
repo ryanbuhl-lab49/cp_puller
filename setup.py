6f88e944331b9decfbc1eb31f35804483add5bb4
ryan.buhl <rsbuhl@gmail.com>
2020-10-02 13:19:49 -0400

    - pin installation requirements

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='cp_pull',
     version='0.1',
     py_modules=['coderpad_pull'],
-    install_requires=['Click',],
+    install_requires=['==7.1.2', ],
     entry_points='''
         [console_scripts]
         cp_pull=coderpad_pull:fetch
